417dc902b71db19e5fe2f1578c998b56ca48e59f
MatveyBaranchugov <mattdav961@gmail.com>
2025-05-04 14:28:20 +0300

    Fix PushFront & Add static initialization with TypeList to other structures

--- Task_2/main.cpp
+++ Task_2/main.cpp
@@ -6,6 +6,9 @@
 //std::true_type — тип, представляющий true (содержит статическое value = true).
 //std::false_type — тип, представляющий false (содержит статическое value = false).
 
+//Проблема: возникает ошибка если в TypeAt добавить структуру TypeList
+//static_assert(std::is_same_v<typename tl::TypeAt<3, myList>::type, double>, "...");
+
 namespace tl{
     // Базовая структура
     template <typename... Types>
@@ -19,7 +22,13 @@ namespace tl{
     //--------------------------------------------------------------
     //Cтруктура для поиска типа по индексу
     template <size_t Index, typename... Types>
-    struct TypeAt;    
+    struct TypeAt; 
+    
+    // Специализация c TypeList
+    template <size_t Index, typename... Types>
+    struct TypeAt<Index, TypeList<Types...>>{
+        using type = typename TypeAt<Index, Types...>::type;
+    };
 
     //Специализация когда Index = 0
     template <typename T, typename... Rest>
@@ -39,6 +48,10 @@ namespace tl{
     template <typename... Types>
     struct Contains;
 
+    // Специализация c TypeList
+    template <typename T, typename... Types>
+    struct Contains<T, TypeList<Types...>> : Contains<T, Types...> {};
+
     //Специализация для пустого списка (наследуем false_type)
     template<typename T>
     struct Contains<T>: std::false_type{};
@@ -56,6 +69,10 @@ namespace tl{
     //Структура для нахождения индекса конкретного типа
     template<typename... Types>
     struct IndexOf;
+
+    // Специализация c TypeList
+    template <typename T, typename... Types>
+    struct IndexOf<T, TypeList<Types...>> : IndexOf<T, Types...>{};
 
     //Специализация, когда искомый индекс следующий
     template<typename T, typename... Rest>
@@ -91,20 +108,19 @@ namespace tl{
     struct PushFront;
 
     template<typename NT, typename... Types>
-    struct PushFront<NT, Types...>{
-        using type = TypeList<NT, TypeList<Types...>>;
+    struct PushFront<NT, TypeList<Types...>>{
+        using type = TypeList<NT, Types...>;
     };
     //--------------------------------------------------------------
-};
-    
+};   
 
 
 
 int main(int argc, char* argv []){
     using myList = tl::TypeList<int, float, char>;
-    using t1 = tl::TypeAt<1, int, float, char>;
-    using t2 = tl::Contains<int, int, float, char>;
-    using ind1 = tl::IndexOf<float, int, float, char>;
+    using t1 = tl::TypeAt<1, myList>; // можно using t1 = tl::TypeAt<1, int, float, char>;
+    using t2 = tl::Contains<int, myList>; // можно using t2 = tl::Contains<int, int, float, char>;
+    using ind1 = tl::IndexOf<float, myList>; // можно using ind1 = tl::IndexOf<float, int, float, char>;
 
     static_assert(std::is_same_v<typename t1::type, float>, "Несовпадение типов");
     static_assert(t2::value, "Тип не содержится в TypeList");
@@ -112,7 +128,7 @@ int main(int argc, char* argv []){
     std::cout << "Ок!" << std::endl;
 
     using newList = tl::PushBack<double, myList>;
-    static_assert(std::is_same_v<tl::TypeAt<3, int, float, char, double>::type, double>, "Ошибка добавления типа");
+    static_assert(std::is_same_v<typename tl::TypeAt<3, int, float, char, double>::type, double>, "...");
 
     return 0;
 }
